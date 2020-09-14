98ae93c2ea8ec80d2e0d0b6193c562e4277ffc33
Stephen Gary <sgary0@protonmail.com>
2020-09-14 15:07:14 -0400

    updated migraton and seeds

--- data/seeds/001-init.js
+++ data/seeds/001-init.js
@@ -1,13 +1,15 @@
+exports.seed = function (knex) {
+  
+  const roles = [
+    {
+      name: "admin", 
+    },
+    {
+      name: "user", 
+    },
+  ];
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex("roles")
+    .insert(roles)
+    .then(() => console.log("\n== Seed data for roles table added. ==\n"));
 };

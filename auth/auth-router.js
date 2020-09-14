7f95f74f4fd0a433db0bf2178eddf7d2ff8d0009
Stephen Gary <sgary0@protonmail.com>
2020-09-14 15:38:10 -0400

    register users

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,35 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'API is up and running' })
+const bcryptjs = require('bcryptjs');
+
+const Users = require('../users/users-model.js');
+
+router.post('/register', (req, res) => {
+    const userInfo = req.body;
+    const isValid = validateUser(userInfo);
+
+    if(isValid) {
+        const rounds = process.env.BCRYPT_ROUNDS || 4;
+        const hash = bcryptjs.hashSync(userInfo.password, rounds);
+        userInfo.password = hash;
+
+        Users.add(userInfo) 
+            .then(inserted => {
+                res.status(201).json({ data: inserted });
+            })
+            .catch(error => {
+                res.status(500).json({ error: error.message });
+            });
+    } else {
+        res.status(400).json({  
+            message: 'Invalid information, please verify and try again' 
+        });
+    }
 });
+
+function validateUser(user) {
+    return user.username && user.password && user.role ? true : false;
+}
 
 module.exports = router;

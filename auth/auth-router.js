f595105973e764b80e806058757424ca240c92ae
Stephen Gary <sgary0@protonmail.com>
2020-09-14 15:01:41 -0400

    knexfile.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({ message: 'Api is up and running' })
+    res.status(200).json({ message: 'API is up and running' })
 });
 
 module.exports = router;
